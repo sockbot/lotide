f984ad7d9b5ea3313f5f367ebd8e6b82c9b2da24
sockbot <github@sockbot.com>
2019-07-23 23:12:09 +0000

    added without function with tests

---
+++ without.js
@@ -0,0 +1,38 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        source.splice(i, 1);
+      }
+    }
+  }
+  // console.log(source);
+  return source;
+};
+
+assertArraysEqual(without([1,2,3], [1]), [2,3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+// assertArraysEqual([1,2,3,4],[1,2,3,4]);
+// assertArraysEqual([4,3,2,1],[1,2,3,4]);

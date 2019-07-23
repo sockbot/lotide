2b06d55094d50ea80f2ecda8142202339328dfd1
sockbot <github@sockbot.com>
2019-07-23 22:21:46 +0000

    created assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+assertArraysEqual([1,2,3,4],[1,2,3,4]);
+assertArraysEqual([4,3,2,1],[1,2,3,4]);

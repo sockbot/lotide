851123f7f8bc126086d73bfa972933b32266af02
sockbot <github@sockbot.com>
2019-07-24 20:34:33 +0000

    letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  console.log(results);
+  return results;
+}
+
+let testString = "hello";
+let expectedOutput = {
+  h: [0],
+  e: [1],
+  l: [2,3],
+  o: [4]
+}
+let actualOutput = letterPositions(testString);
+assertArraysEqual(actualOutput.h, expectedOutput.h);

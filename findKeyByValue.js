f037c8a5cfac1c484eb86c55f5d3e631b7eee11b
sockbot <github@sockbot.com>
2019-07-24 21:05:27 +0000

    findKeyByValue function added

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  let objKeys = Object.keys(obj);
+  let index = 0;
+  for (let element in obj) {
+    if (obj[element] === val) {
+      return objKeys[index];
+    }
+    index++;
+  }
+  return undefined;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

dbb3c9d4d22593ecd5a1d767c2b62c9d11408108
sockbot <github@sockbot.com>
2019-07-29 22:06:44 +0000

    added all helper functions to index.js; paired with @chaiurs

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue')
+const flatten = flatten('./flatten');
 const head = require('./head');
-const tail = require('./tail');
+const letterPositions = require('./head');
+const map = require('./map');
 const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  eqArrays:         eqArrays,
+  eqObjects:        eqObjects,
+  findKey:          findKey,
+  findKeyByValue:   findKeyByValue,
+  flatten:          flatten,
+  head:             head,
+  letterPositions:  letterPositions,
+  map:              map,
+  middle:           middle,
+  tail:             tail,
+  takeUntil:        takeUntil,
+  without:          without
 }

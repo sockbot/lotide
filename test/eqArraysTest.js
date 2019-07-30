19c44a0a068a1fd32a9cf7e1571fb2185f329882
sockbot <github@sockbot.com>
2019-07-30 02:55:24 +0000

    modularized eqArrays and test functions using mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,17 +1,14 @@
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays')
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => true
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+describe("#eqArrays", () => {
 
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-// let result1 = eqArrays([[2, 3], [4]], [[2, 3], [4]]); // => true
-// let result2 = eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]); // => false
-// let result3 = eqArrays([[2, 3], [4]], [[2, 3], 4]); // => false
-// console.log(result1);
-// console.log(result2);
-// console.log(result3);
+  it("should return true for [1,2,3] and [1,2,3]", () => {
+    assert.equal(eqArrays([1,2,3], [1,2,3]), true);
+  });
+  
+  it("should return false for [1,2,3] and [3,2,1]", () => {
+    assert.equal(eqArrays([1,2,3], [3,2,1]), false);
+  });
+    
+});

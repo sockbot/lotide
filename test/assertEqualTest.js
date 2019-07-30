3f64cd1f888d71132a94289d5b40361a06ba516f
sockbot <github@sockbot.com>
2019-07-30 03:25:08 +0000

    un-required assertArraysEqual, assertEqual and assertObjectsEqual because they are harder to test due to using console.log instead of return

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,7 @@
-const assertEqual = require('../assertEqual')
+// const assertEqual = require('../assertEqual')
 
-assertEqual("String", "String");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("1", 1);
-assertEqual(1, 2);
+// assertEqual("String", "String");
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("1", 1);
+// assertEqual(1, 2);
